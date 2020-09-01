66e025a4d8318d94b47cde43a16ac31ba941804b
Gilbert Mwangi <mwangigilbert14@gmail.com>
2020-09-01 18:47:17 -0500

    update

--- src/main/java/com/revature/Driver/DriverApp.java
+++ src/main/java/com/revature/Driver/DriverApp.java
@@ -13,11 +13,16 @@ public class DriverApp {
 	private Scanner cons = new Scanner(System.in);
 	private UserDaoImpl udi = new UserDaoImpl();
 
-	private User currUser = null;
+	private static User currUser = null;
 	
 	public static void main(String args[]) {
-		DriverApp dr = new DriverApp();
-		dr.showMainMenu();
+		DriverApp dr = new DriverApp();	
+		try {
+			dr.showMainMenu();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
 	}
 	
 	public String isvalidInteger(String str) {
@@ -27,7 +32,7 @@ public class DriverApp {
 	}
 	
 	
-	public void showMainMenu() {
+	public void showMainMenu() throws SQLException {
 		boolean valid = false;
 		while(!valid) {
 			System.out.println("Enter 1 to sign into existing account");
@@ -37,11 +42,11 @@ public class DriverApp {
 			int choice = Integer.parseInt(isvalidInteger(cons.nextLine()));
 			switch(choice) {
 			case 1:
-				//login
+				doUserLogin();
 				valid = true;
 				break;
 			case 2:
-				//create account
+				createUser();
 				break;
 			case 3:
 				System.out.println("Thank you for choosing "+bankname);
@@ -62,8 +67,10 @@ public class DriverApp {
 			String password = cons.nextLine();
 			
 			User u = udi.getUserByUserName(username);
-			if(u != null && u.getPassword() == password) {
+			
+			if(u != null && password.equals(u.getPassword())) {
 				currUser = u;
+				isvalid = true;
 			}
 			else {
 				System.out.println("The username and password you have entered dont match existing accounts");
@@ -75,7 +82,7 @@ public class DriverApp {
 				
 				switch(choice) {
 				case 'c':
-					//call create account
+					createUser();
 					break;
 				case 'q':
 					System.out.println("Thank you for choosing "+bankname);
@@ -87,6 +94,85 @@ public class DriverApp {
 				
 			}
 		}	
+	}
+	
+	/*
+	 * Checks the string so that it has no spaces, numbers, or special characters 
+	 * with a limit of 1 to 25 characters
+	 */
+	public boolean nameVal(String str) {
+		Pattern p = Pattern.compile("(?i)(^[a-z]+)[a-z .,-]((?! .,-)$){1,25}$");
+		if(p.matcher(str.toLowerCase()).matches()) return true;
+		return false;
+	}
+	
+	/*
+	 * Validated username:
+	 * valid characters are A-Z, a-z, 0-9, dashes, and underscores
+	 * length 3 - 30 
+	 */
+	public boolean valUsername(String str) {
+		Pattern p = Pattern.compile("[a-zA-Z0-9\\\\._\\\\-]{3,30}");
+		if(p.matcher(str.toLowerCase()).matches()) return true;
+		return false;
+	}
+	
+	
+	public void createUser() throws SQLException {
+		String[] credentials = new String[4];
+		String input = "";
+		boolean isValid = false;
+		//first name 
+		while(!isValid) {
+			System.out.println("Please entert the first name");
+			input = cons.nextLine();
+			if(nameVal(input)) {
+				credentials[0] = input;
+				isValid = true;
+			}else System.out.println("Invalid input, must not containt number or special characters");
+		}
+		//last name
+		isValid = false;
+		while(!isValid) {
+			System.out.println("Please entert the last name");
+
+			input = cons.nextLine();
+			if(nameVal(input)) {
+				credentials[1] = input;
+				isValid = true;
+			}else System.out.println("Invalid input, must not containt number or special characters");
+		}
+		//Username
+		isValid = false;
+		while(!isValid) {
+			System.out.println("Please entert the user name");
+			input = cons.nextLine();
+			
+			
+			if(valUsername(input) && udi.getUserByUserName(input) == null) {
+				credentials[2] = input;
+				isValid = true;
+			}else System.out.println("Invalid input, username is already taken"
+					+ "\n valid characters are A-Z, a-z, 0-9, dashes, and underscores");
+		}
+		
+		isValid = false;
+		while(!isValid) {
+			System.out.println("Please entert the password");
+			input = cons.nextLine();
+			if(input.length() < 30  && input.length() > 0) {
+				credentials[3] = input;
+				isValid = true;
+			}else System.out.println("Invalid input, you know what you did");
+		}
+		
+		String checkPass = "";
+		while (!checkPass.equals(credentials[3])) {
+			System.out.println("Please verify your password entered: ");
+			checkPass = cons.nextLine();
+		}
+		//create account
+		udi.createUser(new User(0,credentials[0],credentials[1],credentials[2],credentials[3],1));
 	}
 }
 
