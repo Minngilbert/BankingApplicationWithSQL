c38189283c40fc801b4bff77683b94cc1a764a23
Gilbert Mwangi <mwangigilbert14@gmail.com>
2020-09-01 20:08:40 -0500

    update

--- src/main/java/com/revature/Driver/DriverApp.java
+++ src/main/java/com/revature/Driver/DriverApp.java
@@ -1,38 +1,37 @@
 package com.revature.Driver;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
+import com.revature.DaoImpl.AccountDaoImpl;
 import com.revature.DaoImpl.UserDaoImpl;
+import com.revature.beans.Account;
 import com.revature.beans.User;
 
 public class DriverApp {
 	
 	private String bankname = "Pokeman Bank";
 	private Scanner cons = new Scanner(System.in);
-	private UserDaoImpl udi = new UserDaoImpl();
 
+	private UserDaoImpl udi = new UserDaoImpl();
+	private AccountDaoImpl adi = new AccountDaoImpl();
+	
 	private static User currUser = null;
 	
 	public static void main(String args[]) {
 		DriverApp dr = new DriverApp();	
 		try {
-			dr.showMainMenu();
+			dr.launchMenu();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}	
 	}
-	
-	public String isvalidInteger(String str) {
-		Pattern p = Pattern.compile("[0-9]+");
-		if(p.matcher(str).matches()) return str;
-		return "0";
-	}
-	
-	
-	public void showMainMenu() throws SQLException {
+
+	//This method is run on start up
+	public void launchMenu() throws SQLException {
 		boolean valid = false;
 		while(!valid) {
 			System.out.println("Enter 1 to sign into existing account");
@@ -53,11 +52,13 @@ public class DriverApp {
 				valid = true;
 				break;
 			default:
-				System.out.println("Invalid, try again");
+				System.out.println("Invalid input, try again");
 			}
 		}
 	}
 	
+
+
 	public void doUserLogin() throws SQLException {
 		boolean isvalid = false;
 		while(!isvalid) {
@@ -96,6 +97,13 @@ public class DriverApp {
 		}	
 	}
 	
+	//Used in main menu to validate integer
+	public String isvalidInteger(String str) {
+		Pattern p = Pattern.compile("[0-9]+");
+		if(p.matcher(str).matches()) return str;
+		return "0";
+	}
+	
 	/*
 	 * Checks the string so that it has no spaces, numbers, or special characters 
 	 * with a limit of 1 to 25 characters
@@ -117,7 +125,7 @@ public class DriverApp {
 		return false;
 	}
 	
-	
+	//Create a new user account
 	public void createUser() throws SQLException {
 		String[] credentials = new String[4];
 		String input = "";
@@ -173,6 +181,129 @@ public class DriverApp {
 		}
 		//create account
 		udi.createUser(new User(0,credentials[0],credentials[1],credentials[2],credentials[3],1));
+	}
+	
+	//Shows normal user account menu
+	public void userMenu() throws SQLException {
+		ArrayList<Account> accounts = adi.getAccountsByUserName(currUser.getUserName());
+		char choice ;
+		boolean isValid = false;
+		int accountId;
+		while(isValid) {
+			System.out.println("Enter V to view active bank accounts");
+			System.out.println("Enter D to make a deposit");
+			System.out.println("Enter W to withdraw funds");
+			System.out.println("Enter C to create new account");
+			System.out.println("Enter q to log out");
+			
+			if(accounts.size() > 1) {
+				
+				choice = cons.nextLine().toLowerCase().charAt(0);
+				switch(choice) {
+				case 'v':
+					for(Account a: accounts) {
+						System.out.println(a.toString());
+					}
+					break;
+				case 'd':
+					System.out.println("Please enter the account number you wish to depoist to");
+					accountId = Integer.parseInt(isvalidInteger(cons.nextLine()));
+					if(accountId == 0) {
+						System.out.println("The account entered does not exist");
+						break;
+					}else doDeposit(accountId);
+					break;
+				case 'w':
+					
+					System.out.println("Please enter the account number you wish to withdraw from");
+					accountId = Integer.parseInt(isvalidInteger(cons.nextLine()));
+					
+					if(accountId == 0) {
+						System.out.println("The account entered does not exist");
+						break;
+					}
+					for(Account a: accounts) {
+						if(a.getAccountId() != accountId) {
+							System.out.println("The account entered does not belong to you");
+						}
+					}	
+					doWithdrawal(accountId);
+					break;
+				case 'c':
+					createAccount(currUser.getUserName());
+					accounts = adi.getAccountsByUserName(currUser.getUserName());
+					break;
+				case 'q':
+					isValid = true;
+					break;
+				}
+				
+				
+			}
+			else {
+				System.out.println("No bank accounts to act on, enter y to create new bank account ");
+				choice = cons.nextLine().toLowerCase().charAt(0);
+				
+				switch(choice) {
+				case 'y':
+					createUser();
+					break;
+				default:
+					System.out.println("No actions available, login out");
+					isValid = true;
+				}	
+			}
+		}
+	}
+	
+	//creates a new bank account for the user and validates that the user has less than 3 accounts
+	public void createAccount(String accountUserName) throws SQLException {
+		ArrayList<Account> accounts = adi.getAccountsByUserName(accountUserName);
+		if(accounts.size() < 3) {
+			adi.createAccount(udi.getUserByUserName(accountUserName));
+		}else System.out.println("Limit of 2 accounts");
+	}
+	
+	public void doDeposit(int accountId) throws SQLException {
+		Account a = adi.getAccountByAccountId(accountId);
+		boolean isValid = false;	
+		double amount;
+		
+		while(!isValid) {
+			System.out.println("Please enter the amount you wish to deposit");
+			amount = Double.parseDouble(cons.nextLine());
+			if (amount < 0.01) {
+				System.out.println("Cannot deposit less than one cent.");
+			}else {
+				isValid = adi.updateAccountbalance(accountId, amount+a.getBalance());
+				a = adi.getAccountByAccountId(accountId);
+				System.out.println(
+						"Successfully deposited $" + amount + " to account.  New balance is $" + a.getBalance());
+			}
+			
+		}
+	}
+
+	public void doWithdrawal(int accountId) throws SQLException {
+		Account a = adi.getAccountByAccountId(accountId);
+		boolean isValid = false;	
+		double amount;
+		
+		while(!isValid) {
+			System.out.println("Please enter the amount you wish to withdraw or q to exit");
+			amount = Double.parseDouble(cons.nextLine());
+			if (amount < 0.01) {
+				System.out.println("Cannot withdraw less than one cent.");
+			} else if (amount > a.getBalance()) {
+				System.out.println("Cannot overdraw an account.");
+			}else {
+				isValid = adi.updateAccountbalance(accountId, amount - a.getBalance());
+				a = adi.getAccountByAccountId(accountId);
+				System.out.println(
+						"Successfully withdrew $" + amount + " to account.  New balance is $" + a.getBalance());
+			
+			}
+		}
 	}
 }
 
